99996de66d0243bceb1f75dfb3faa628f2fac8bb
vitalii_kim <vitaliikim992@gmail.com>
2019-12-16 17:45:51 +0400

    final

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -1,3 +1,3 @@
 chcp 65001
 taskkill /f /im excel.exe
-cscript \\epam.com\Projects\Petersburg\GZAM-APNT\temp\script.vbs "\\epam.com\Projects\Petersburg\GZAM-APNT\temp\Регистрация_30.08.13_test.xlsx"
+cscript \\epam.com\Projects\Petersburg\GZAM-APNT\temp\script.vbs "\\epam.com\Projects\Petersburg\GZAM-APNT\temp\Регистрация_30.08.13.xlsx"
